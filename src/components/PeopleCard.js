a3d969ecc193e88352ad12355c041dfd5978d434
David Choi <45525219+dyc-github@users.noreply.github.com>
2023-04-11 10:28:10 -0700

    staff cards finished

--- src/components/PeopleCard.js
+++ src/components/PeopleCard.js
@@ -1,23 +1,53 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 const PeopleCard = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
   return (
-    <Card sx={{flexDirection: 'column',}}>
-      {/* <CardMedia component="img"
-        height="194"
-        image={require("../assets/images/grant-participants/Sankja Banerjee.jpeg")}
-        alt="person"/> */}
-      <CardContent>
-        <Typography>
-          Name
-        </Typography>
-        <Typography>
-          Title
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <ReactCardFlip isFlipped={isFlipped}>
+      <Card className="people_card">
+        <CardActionArea
+          onClick={() => {
+            setIsFlipped(!isFlipped);
+          }}
+        >
+          <CardMedia
+            component="img"
+            image={require("../assets/images/grant-participants/Sankja Banerjee.jpeg")}
+            alt="person"
+          />
+          <CardContent>
+            <Typography variant="h5">Fred Adams</Typography>
+            <Typography variant="p">Professional Baker</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+      <Card className="people_card">
+        <CardActionArea
+          onClick={() => {
+            setIsFlipped(!isFlipped);
+          }}
+        >
+          <CardContent>
+            <Typography variant="h5">Fred Adams</Typography>
+            <Typography variant="p" >Professional Baker <br/></Typography>
+            <Typography variant="p">
+              A workshop series inaugural event to learn about technology,
+              Computer Science, networking and research. This is a free
+              conference, open to Central Valley students. Discover paid
+              research opportunities and apply at this event. Register here.
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </ReactCardFlip>
   );
 };
 
--- src/pages/People.js
+++ src/pages/People.js
@@ -1,6 +1,6 @@
 
 import Navbar from '../components/Navbar'
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { Container } from '@mui/system';
 import PeopleCard from '../components/PeopleCard';
 
@@ -9,12 +9,23 @@ const People = () => {
         <div>
             <Navbar />
             <Container>
-                <Typography component="h1">
-                    Our Team
-                </Typography>
-                <PeopleCard/>
+                <Box className="header" sx={{margin: '3rem 0'}}>
+                    <Typography sx={{width:'fit-content', margin:'auto'}} variant='h4' component='h1'>
+                        Our Team
+                    </Typography>
+                </Box>
+                <Box sx={{display:'flex', justifyContent:'space-between', flexWrap:'wrap', gap:4}}>
+                    <PeopleCard/>
+                    <PeopleCard/>
+                    <PeopleCard/>
+                    <PeopleCard/>
+                    <PeopleCard/>
+                    <PeopleCard/>
+                    <PeopleCard/>
+
+                </Box>
+                
             </Container>
-            
         </div>
     );
 }
