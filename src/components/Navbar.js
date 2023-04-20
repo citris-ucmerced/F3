5afdf1302e47316aa5b20a8b7ea2e344e3d61074
Alonso Ortiz <alonsoortiz73@gmail.com>
2023-04-20 12:08:30 -0700

    Sidebar menu implemented for mobile view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,66 @@
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box, Button, List, ListItem, ListItemButton, ListItemText, SwipeableDrawer } from "@mui/material";
+import { useState } from 'react';
+
 import MenuIcon from "@mui/icons-material/Menu";
 import logo from "../assets/images/F3_Logo.png";
 
 const Navbar = () => {
+  const routes = [
+    {
+      name: 'Home',
+      route: '/'
+    },
+    {
+      name: 'News',
+      route: '/News'
+    },
+    {
+      name: 'Challenge Grants',
+      route: '/ChallengeGrants'
+    },
+    {
+      name: 'People',
+      route: '/People'
+    },
+    {
+      name: 'Contact Us',
+      route: '/ContactUs'
+    }
+  ];
+
+  const SideBar = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {routes.map((route) => (
+          <ListItem key={route.name} disablePadding>
+            <ListItemButton component={Link} to={route.route}>
+              <ListItemText primary={route.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setOpen(open);
+  };
 
   return (
     <AppBar className="main-navbar">
@@ -13,25 +70,26 @@ const Navbar = () => {
         </Box>
         <Typography sx={{ flexGrow: 1, fontWeight: 'bold' }}>F3 Innovate</Typography>
 
-        <Button>
+        <Button onClick={toggleDrawer(true)}>
           <MenuIcon id="hamburger-menu" />
         </Button>
 
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/News" className="nav-link">
-          News
-        </Link>
-        <Link to="/ChallengeGrants" className="nav-link">
-          Challenge Grants
-        </Link>
-        <Link to="/People" className="nav-link">
-          People
-        </Link>
-        <Link to="/ContactUs" className="nav-link">
-          Contact Us
-        </Link>
+        <SwipeableDrawer
+          anchor="right"
+          open={open}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+        >
+          <SideBar />
+        </SwipeableDrawer>
+
+        {
+          routes.map((route) => (
+            <Link to={route.route} className="nav-link" key={route.name}>
+              {route.name}
+            </Link>
+          ))
+        }
 
       </Toolbar>
     </AppBar>
