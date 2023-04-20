fcc7e513f83c06e9ffa7d1aedaf40592a6204970
Alonso Ortiz <alonsoortiz73@gmail.com>
2023-04-20 13:53:14 -0700

    Update styling

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -6,17 +6,20 @@ import {
   Typography,
 } from "@mui/material";
 
-const NewsCard = ({ title, description, imageName, tags }) => {
+const NewsCard = ({ title, description, imageName }) => {
+
   const NEWS_IMAGE_PATH = "./images/news/";
   const newsImage = NEWS_IMAGE_PATH + imageName;
 
   return (
     <Card className="news-card">
       <CardMedia component="img" image={newsImage} />
-      <Typography variant="h5">{title}</Typography>
-      <Typography variant="body2" className="news-card-summary">
-        {description}
-      </Typography>
+      <div className="news-card-text">
+        <Typography variant="h5">{title}</Typography>
+        <Typography variant="body2" className="news-card-description">
+          {description}
+        </Typography>
+      </div>
     </Card>
   );
 };
