e72ef88ccfc90fd11e8af12dca7316ee49ca3ce9
Alonso Ortiz <103380838+alonso-ov@users.noreply.github.com>
2023-05-16 10:39:31 -0700

    Removed LinkedIn account, and fixed typos in Challenge Grants summaries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
 import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer = () => {
 
@@ -14,9 +13,6 @@ const Footer = () => {
 
             <a href="https://twitter.com/f3ucmerced">
               <TwitterIcon style={{ fill: "black" }}/>
-            </a>
-            <a href="https://www.linkedin.com/company/center-for-information-technology-research-in-the-interest-of-society-citris-/">
-              <LinkedInIcon style={{ fill: "black" }}/>
             </a>
         </Toolbar>
     </AppBar>
