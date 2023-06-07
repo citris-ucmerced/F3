542bfff564ce9c8492a84ed1be72089fc172e29a
Alonso Ortiz <103380838+alonso-ov@users.noreply.github.com>
2023-06-07 10:19:31 -0700

    Added blur to custom-card wrapped inside of ReactCardFlip with divs

--- src/components/PeopleCard.js
+++ src/components/PeopleCard.js
@@ -16,61 +16,65 @@ const PeopleCard = ({ name, title, description, fileName }) => {
   const image = PEOPLE_IMAGE_PATH + fileName;
 
   return (
-    <ReactCardFlip isFlipped={isFlipped}>
-      <Card className="people-card custom-card">
-        <CardActionArea
-          disableRipple={true}
-          onClick={() => {
-            setIsFlipped(!isFlipped);
-          }}
-        >
-          <CardMedia
-            component="img"
-            draggable={false}
-            image={image}
-            alt="person"
-          />
-          <CardContent>
-            <Typography className="card-title" variant="h5">
-              {name}
-            </Typography>
-            <Typography className="card-subtitle" variant="p">
-              {title}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-
-      <Card className="people-card custom-card">
-        <CardActionArea
-          disableRipple={true}
-          onClick={() => {
-            setIsFlipped(!isFlipped);
-          }}
-        >
-          <CardContent>
-            <Typography className="card-title" variant="h5">
-              {name}
-            </Typography>
-            <Typography className="card-subtitle" variant="p">
-              {title}
-            </Typography>
-            <br />
-            <div
-              className="people_card_description"
-              style={{
-                marginTop: '1rem',
-                maxHeight: "15.5rem", // Adjust the height as per your requirements
-                overflowY: "auto",
-              }}
-            >
-              <Typography className="people_card_description" variant="p">
-                {description}
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div>
+        <Card className="people-card custom-card">
+          <CardActionArea
+            disableRipple={true}
+            onClick={() => {
+              setIsFlipped(!isFlipped);
+            }}
+          >
+            <CardMedia
+              component="img"
+              draggable={false}
+              image={image}
+              alt="person"
+            />
+            <CardContent>
+              <Typography className="card-title" variant="h5">
+                {name}
               </Typography>
-            </div>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+              <Typography className="card-subtitle" variant="p">
+                {title}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
+
+      <div>
+        <Card className="people-card custom-card">
+          <CardActionArea
+            disableRipple={true}
+            onClick={() => {
+              setIsFlipped(!isFlipped);
+            }}
+          >
+            <CardContent>
+              <Typography className="card-title" variant="h5">
+                {name}
+              </Typography>
+              <Typography className="card-subtitle" variant="p">
+                {title}
+              </Typography>
+              <br />
+              <div
+                className="people_card_description"
+                style={{
+                  marginTop: '1rem',
+                  maxHeight: "15.5rem", // Adjust the height as per your requirements
+                  overflowY: "auto",
+                }}
+              >
+                <Typography className="people_card_description" variant="p">
+                  {description}
+                </Typography>
+              </div>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </div>
     </ReactCardFlip>
   );
 };
