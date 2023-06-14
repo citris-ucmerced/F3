1cf80e37502e1824bb06f034c067963beb057b18
Alonso Ortiz <103380838+alonso-ov@users.noreply.github.com>
2023-06-14 10:24:34 -0700

    Uninstall react-card-flip Flipping cards now use pure CSS

--- src/components/PeopleCard.js
+++ src/components/PeopleCard.js
@@ -6,7 +6,6 @@ import {
   Typography,
 } from "@mui/material";
 import { useState } from "react";
-import ReactCardFlip from "react-card-flip";
 
 const PEOPLE_IMAGE_PATH = "./images/people/";
 
@@ -16,66 +15,68 @@ const PeopleCard = ({ name, title, description, fileName }) => {
   const image = PEOPLE_IMAGE_PATH + fileName;
 
   return (
-    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-      <div>
-        <Card className="people-card custom-card">
-          <CardActionArea
-            disableRipple={true}
-            onClick={() => {
-              setIsFlipped(!isFlipped);
-            }}
-          >
-            <CardMedia
-              component="img"
-              draggable={false}
-              image={image}
-              alt="person"
-            />
-            <CardContent>
-              <Typography className="card-title" variant="h5">
-                {name}
-              </Typography>
-              <Typography className="card-subtitle" variant="p">
-                {title}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </div>
-
-      <div>
-        <Card className="people-card custom-card">
-          <CardActionArea
-            disableRipple={true}
-            onClick={() => {
-              setIsFlipped(!isFlipped);
-            }}
-          >
-            <CardContent>
-              <Typography className="card-title" variant="h5">
-                {name}
-              </Typography>
-              <Typography className="card-subtitle" variant="p">
-                {title}
-              </Typography>
-              <br />
-              <div
-                className="people_card_description"
-                style={{
-                  marginTop: '1rem',
-                  maxHeight: "15.5rem", // Adjust the height as per your requirements
-                  overflowY: "auto",
-                }}
-              >
-                <Typography className="people_card_description" variant="p">
-                  {description}
+    <div className="flip-card">
+      <div className={`flip-card-inner ${isFlipped ? "do-flip": ""}`}>
+        <div className="flip-card-front">
+          <Card className="people-card custom-card">
+            <CardActionArea
+              disableRipple={true}
+              onClick={() => {
+                setIsFlipped(!isFlipped);
+              }}
+            >
+              <CardMedia
+                component="img"
+                draggable={false}
+                image={image}
+                alt="person"
+              />
+              <CardContent>
+                <Typography className="card-title" variant="h5">
+                  {name}
                 </Typography>
-              </div>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+                <Typography className="card-subtitle" variant="p">
+                  {title}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
+
+        <div className="flip-card-back">
+          <Card className="people-card custom-card">
+            <CardActionArea
+              disableRipple={true}
+              onClick={() => {
+                setIsFlipped(!isFlipped);
+              }}
+            >
+              <CardContent>
+                <Typography className="card-title" variant="h5">
+                  {name}
+                </Typography>
+                <Typography className="card-subtitle" variant="p">
+                  {title}
+                </Typography>
+                <br />
+                <div
+                  className="people_card_description"
+                  style={{
+                    marginTop: "1rem",
+                    maxHeight: "15.5rem", // Adjust the height as per your requirements
+                    overflowY: "auto",
+                  }}
+                >
+                  <Typography className="people_card_description" variant="p">
+                    {description}
+                  </Typography>
+                </div>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
       </div>
-    </ReactCardFlip>
+    </div>
   );
 };
 
