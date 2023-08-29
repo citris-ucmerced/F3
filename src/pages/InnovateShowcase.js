b2c5b8e3de125424e6f657337085f8ef7ac8000f
aravgupta-dev <agupta59@ucmerced.edu>
2023-08-29 12:47:13 -0700

    Added Final Touches to innovate Showcase page

--- src/pages/InnovateShowcase.js
+++ src/pages/InnovateShowcase.js
@@ -6,6 +6,7 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  Link,
 } from "@mui/material";
 import { Helmet } from "react-helmet-async";
 
@@ -16,72 +17,112 @@ import "./styles/InnovateShowcase.css"
 
 const InnovateShowcase = () => {
   const agenda = [
+    // existing items and additional items
+    // ... 
     {
-      time: "10:00",
+      time: "10:00am",
       title: "Coffee and Networking",
       details: ""
     },
     {
-      time: "10:15",
+      time: "10:15am",
       title: "Welcome and Introductions",
-      details: "Josh Viers, UC Merced\nRam Nunna, Fresno State"
+      details: "Josh Viers, Associate Vice Chancellor for Research and Strategic Initiatives, UC Merced Ram Nunna, Dean of the Lyles School of Engineering, Fresno State  "
     },
     {
-      time: "10:30",
+      time: "10:30am",
       title: "Technological Innovation for Agriculture: F3 Challenge Grant Research Collaborations ",
-      details: ""
+      details: " - “Smart and advanced water treatment and food processing technologies and strategies: towards the development of sustainable and  controlled environmental agricultural pathways“ \n   - “California’s Water for Farms:  Irrigation Decisions Under Climate Change”\n  - “Non-invasive Hybrid Sensing of Key Chemical Compositions of Grapes” \n  - “Using biochar co-compost to foster a climate smart, circular agriculture economy”"
     },
     {
-      time: "11:30",
+      time: "11:30am",
       title: "Break",
       details: ""
     },
     {
-      time: "11:45",
-      title: "Inclusive Innovation with Industry and Communities Moderated by Dr. Leigh Bernacchi, Executive Director, VISTA F3 Innovate Panelists:  ",
-      details: ""
+      time: "11:45am",
+      title: "Inclusive Innovation with Industry and Communities Moderated by Dr. Leigh Bernacchi, Executive Director, VISTA F3 Innovate Panelists  ",
+      details: " - Dr. Luca Brillante, Assistant Professor of Viticulture, Bronco Wine Co. Chair in Viticulture, Fresno State \n  - Dr. Reza Ehsani, Professor of Mechanical Engineering, UC Merced \n - Dr. Tea Lempiala, Assistant Professor of Management, UC Merced "
+
     },
     {
-      time: "12:25",
+      time: "12:25pm",
       title: "Lunch buffet and networking ",
       details: ""
     },
-    
-    // Add more items
+    {
+      time: "1:10pm",
+      title: "Keynote Speaker: Brad Abraham, Verdant Robotics",
+      details: "Current issues in ag tech innovation"
+    },
+    {
+      time: "2:30pm",
+      title: "Pitches from Challenge Grants and GSRs poster presenters",
+      details: "Poster session to follow\nNetworking and Happy Hour"
+    },
+    {
+      time: "3:00pm",
+      title: "Conclude formal program",
+      details: ""
+    },
   ];
 
   return (
     <>
       <Helmet>
-        <title>InnovateShowcase</title>
+        <title>F3 Innovate Showcase</title>
         <meta name="description" content="upcoming innovate event" />
-        <link rel="canonical" href="/DailySchedule" />
+        <link rel="canonical" href="/InnovateShowcase" />
       </Helmet>
+
       <Box className="page">
         <Navbar />
         <Container>
           <Box my={4}>
-            <Typography variant="h4" component="h1" align="center" className="page-title" gutterBottom>
-              Detailed Agenda
+            <Typography variant="h2" component="h1" align="center" className="page-title" gutterBottom>
+              F3 Innovate Showcase
+            </Typography>
+            <Typography variant="h5" component="h2" align="center" gutterBottom>
+              September 1, 2023, 10a-3p
+            </Typography>
+            <Typography variant="h6" component="h3" align="center" gutterBottom>
+              UC Merced Conference Center Ballroom
+            </Typography>
+            <Typography variant="body1" align="center">
+              Complimentary parking reserved under F3 Innovate in Bellevue Gold Lot: <Link href="https://goo.gl/maps/UbEaRzQiczdD7DD6A">Google Map</Link>
+            </Typography>
+            <Typography variant="body1" align="center">
+              Coach transportation from Fresno State Available: Contact Dean Ram Nunna
+            </Typography>
+            <Typography variant="body1" align="center">
+              F3 Innovate is the nation’s hub for climate-smart agrifood technology and engineering.
             </Typography>
 
-            <Box mt={4} p={2} className="agenda">
-              <Table className="agenda-table">
-                <TableBody>
-                  {agenda.map((item, index) => (
-                    <TableRow key={index} className="agenda-row">
-                      <TableCell className="agenda-time">{item.time}</TableCell>
-                      <TableCell className="agenda-title">
-                        <Typography variant="h6">{item.title}</Typography>
-                        <Typography variant="body2">{item.details.split('\n').map((line, i) => <p key={i}>{line}</p>)}</Typography>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Box>
-        </Container>
+            <Box mt={4}>
+              <Typography variant="h4" component="h1" align="center" className="page-title" gutterBottom>
+                Detailed Agenda
+              </Typography>
+
+              <Box mt={4} p={2} className="agenda">
+                <Table className="agenda-table">
+                  <TableBody>
+                    {agenda.map((item, index) => (
+                      <TableRow key={index} className="agenda-row">
+                        <TableCell className="agenda-time">{item.time}</TableCell>
+                        <TableCell className="agenda-title">
+                          <Typography variant="h6">{item.title}</Typography>
+                          <Typography variant="body2">
+                            {item.details.split('\n').map((line, i) => <p key={i}>{line}</p>)}
+                          </Typography>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Box>
+            </Box>  
+            </Box>       
+          </Container>
         <Footer />
       </Box>
     </>
