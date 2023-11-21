828c813faf76d3713958c2e4b975361fee530dfc
luciabarboza <lucia.barboza443@gmail.com>
2023-11-21 10:03:11 -0800

    Added robotics challenge event with /farmbot URL and completed news page URL

--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Navbar from "../components/Navbar.js";
-import Footer from "../components/Footer.js";
+import Navbar from '../components/Navbar.js';
+import Footer from '../components/Footer.js';
 import newsCSV from '../assets/sheets/news.csv';
-import "./styles/NewsDetail.css";
-import { getRowByTitle } from '../utils/CSVReader.js'; 
+import './styles/NewsDetail.css';
+import { getRowByTitle } from '../utils/CSVReader.js';
 
 const NewsDetail = () => {
   const { slug } = useParams(); // Retrieve the slug from URL
   const [newsItem, setNewsItem] = useState(null);
+
 
   useEffect(() => {
     getRowByTitle(newsCSV, slug, setNewsItem); // Fetch the news item by the slug
@@ -19,15 +20,18 @@ const NewsDetail = () => {
   }
 
 
+
+
   return (
     <>
       <Navbar />
-      <div className='news-detail'>
-        <h1 className='detail-title'>{newsItem.title}</h1>
-        <h3 className='detail-author-name'>Author: {newsItem.author}</h3>
-        <h3 className='detail-author-position'>{newsItem.position}</h3>
-        <p className='detail-description'>{newsItem.description}</p>
-        
+      <div className="page">
+        <div className='news-detail'>
+          <h1 className='detail-title'>{newsItem.title}</h1>
+          <h3 className='detail-author-name'>Author: {newsItem.author}</h3>
+          <h3 className='detail-author-position'>{newsItem.position}</h3>
+          <p className='detail-description'>{newsItem.description}</p>
+        </div>
       </div>
       <Footer />
     </>
